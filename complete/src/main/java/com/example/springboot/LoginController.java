dc320cdacbc82f9c75e57bc2e48766b5e7dd03a0
Aleksander <tatrun89@gmail.com>
2021-03-30 17:55:28 +0300

    test app

--- complete/src/main/java/com/example/springboot/LoginController.java
+++ complete/src/main/java/com/example/springboot/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
+public class LoginController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring!";
 	}
 
 }
