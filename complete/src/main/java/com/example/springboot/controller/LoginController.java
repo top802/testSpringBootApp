f7e9bb614c353261981d4ca78d04e62a87cdd1a6
Alexander <tatrun89@gmail.com>
2021-04-05 15:04:48 +0300

    added another html pages and set mapping to them

--- complete/src/main/java/com/example/springboot/controller/LoginController.java
+++ complete/src/main/java/com/example/springboot/controller/LoginController.java
@@ -10,10 +10,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-	@GetMapping("/login")
-	public String login(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model){
+	@GetMapping("/pages/forgot-password")
+	public String forgotPassword(@RequestParam(name="name", required=false, defaultValue="PASSWORD") String name, Model model){
+		model.addAttribute("name", name);
+		return "forgotPassword";
+	}
+
+	@GetMapping("/pages/history")
+	public String history(@RequestParam(name="name", required=false, defaultValue="HISTORY") String name, Model model){
+		model.addAttribute("name", name);
+		return "history";
+	}
+
+	@GetMapping("/pages/home")
+	public String home(@RequestParam(name="name", required=false, defaultValue="HOME") String name, Model model){
+		model.addAttribute("name", name);
+		return "home";
+	}
+
+	@GetMapping("/pages/login")
+	public String login(@RequestParam(name="name", required=false, defaultValue="LOGIN") String name, Model model){
 		model.addAttribute("name", name);
 		return "login";
+	}
+
+	@GetMapping("/pages/sign-up")
+	public String signUp(@RequestParam(name="name", required=false, defaultValue="SignUP") String name, Model model){
+		model.addAttribute("name", name);
+		return "signUp";
 	}
 
 }
